bb4cd2b111650e8fdc8cf14ae7a1b79fdde8ef3f
Mathias Daniel Hahner <mathias.hahner@cwi.com.br>
2022-07-08 15:57:12 -0300

    added initial logic for game

--- src/ui/components/positions/positions.component.jsx
+++ src/ui/components/positions/positions.component.jsx
@@ -1,20 +1,45 @@
 import './positions.style.css'
 
-const Position = ({ value }) => {
-  return (
-    <li className='position'>
-      {value == 'X' && <div className='X'></div>}
-      {value == 'O' && <div className='O'></div>}
-    </li>
-  )
+import { useState } from 'react'
+
+const INITIAL_GAME = {
+  hasWinner: false,
+  currentPlayer: 'X',
+  positions: ['', '', '', '', '', '', '', '', ''],
 }
 
-export const Positions = ({ game }) => {
+export const Positions = () => {
+  const [game, setGame] = useState(INITIAL_GAME)
+
+  const executeMove = position => {
+    const positions = updatePositions(position)
+    const currentPlayer = togglePlayer()
+    const hasWinner = verifyWinner()
+
+    setGame({ currentPlayer, positions, hasWinner })
+  }
+
+  const updatePositions = position =>
+    game.positions.map((value, index) => (index === position ? game.currentPlayer : value))
+
+  const togglePlayer = () => (game.currentPlayer == 'X' ? 'O' : 'X')
+
+  const verifyWinner = () => verifySequence(1) || verifySequence(2) || verifySequence(3) || verifySequence(4)
+
+  const verifySequence = interval => game.positions.forEach(position => false)
+
   return (
-    <ul className='positions-list'>
-      {game.map(value => (
-        <Position value={value} />
-      ))}
-    </ul>
+    <>
+      {!!game.hasWinner && <h1>ACABOU!!!</h1>}
+
+      <ul className='positions-list'>
+        {game.positions.map((value, position) => (
+          <li className='position' key={position} onClick={() => executeMove(position)}>
+            {value == 'X' && <div className='X' />}
+            {value == 'O' && <div className='O' />}
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
--- src/ui/screens/game/game.screen.jsx
+++ src/ui/screens/game/game.screen.jsx
@@ -3,12 +3,10 @@ import './game.style.css'
 import { Positions } from '../../components'
 
 export const GameScreen = () => {
-  const game = ['X', 'X', 'X', 'O', 'X', 'O', 'X', 'X', 'O']
-
   return (
     <div className='game-vertical '>
       <div className='game-horizontal'>
-        <Positions game={game} />
+        <Positions />
       </div>
     </div>
   )
