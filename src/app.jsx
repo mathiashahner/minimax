d4526f5cb95e64399dd32ba1613220c951d82e79
Mathias Hahner <mathias_hahner@hotmail.com>
2023-03-05 16:44:15 -0300

    stylization and refactor in the gameover screen

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,19 @@
 import './app.css'
 
 import { useState } from 'react'
+import { GameOverScreen } from './ui/screens'
 import { Header, Game } from './ui/components'
 import { INITIAL_GAME, GAME_STATE } from './core'
-import { GameOverScreen } from './ui/screens'
 
 const App = () => {
   const [game, setGame] = useState(INITIAL_GAME)
-
-  const restartGame = () => setGame(INITIAL_GAME)
 
   return (
     <div className='container'>
       <Header />
       <Game game={game} setGame={setGame} />
 
-      {game.gameState === GAME_STATE.TIE && <GameOverScreen restartGame={restartGame} />}
-
-      {game.gameState === GAME_STATE.WINNER && (
-        <GameOverScreen winner={game.currentPlayer} restartGame={restartGame} />
-      )}
+      {game.gameState !== GAME_STATE.PROGRESS && <GameOverScreen game={game} setGame={setGame} />}
     </div>
   )
 }
--- src/ui/screens/game-over/game-over.screen.jsx
+++ src/ui/screens/game-over/game-over.screen.jsx
@@ -1,21 +1,18 @@
 import './game-over.style.css'
 
 import { Modal } from '../../components'
+import { GAME_STATE, INITIAL_GAME } from '../../../core'
 
-export const GameOverScreen = ({ winner, restartGame }) => {
+export const GameOverScreen = ({ game, setGame }) => {
+  const restartGame = () => setGame(INITIAL_GAME)
+
   return (
     <Modal>
-      <h1>GAME OVER!</h1>
-
-      {!!winner ? <p>{winner} WINNER</p> : <p>TIE</p>}
+      {game.gameState === GAME_STATE.TIE ? <h1>XO TIE</h1> : <h1>{game.currentPlayer} WINNER</h1>}
 
       <button className='game-over-btn' onClick={restartGame}>
         PLAY AGAIN
       </button>
     </Modal>
   )
-}
-
-GameOverScreen.defaultProps = {
-  winner: '',
 }
